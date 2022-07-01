b8b638c18dabc576ae7fc19290f47c76466136da
Duniya <duniyamohamedsaleem8@gmail.com>
2022-06-30 23:59:25 -0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome to html page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique nesciunt minus voluptas pariatur earum consectetur fugiat debitis est nulla numquam! Sed a vero recusandae quia quisquam repudiandae alias illo exercitationem.</p>
+<img src="{{asset('images/download.jpg')}}" alt="no image found">
+
+    
+</body>
+</html>
